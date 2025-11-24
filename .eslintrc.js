b9e7a1f556afee912973769b1b26b268178f57bd
dawan <ifdawanprintqualified14@gmail.com>
2025-11-24 15:58:06 -0500

    fix: configure ESLint to properly parse ES6 modules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,1 +1,24 @@
- 
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  extends: [
+    'react-app',
+    'react-app/jest',
+  ],
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
